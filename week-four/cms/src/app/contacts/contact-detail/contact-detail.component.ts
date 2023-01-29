a8087f7bcf7959c7cd4352206af4fc61199c2786
keviner99 <90742482+keviner99@users.noreply.github.com>
2023-01-28 20:09:07 -0500

    implement document item

---
+++ week-four/cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.scss'],
+})
+export class ContactDetailComponent implements OnInit {
+  
+  @Input() contact: Contact;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+}

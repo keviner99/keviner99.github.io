fe7ecd9df3605a04510b3b83d2dfbeed1b3c38c9
keviner99 <90742482+keviner99@users.noreply.github.com>
2023-01-28 20:28:40 -0500

    implement the document detail

--- week-four/cms/src/app/documents/document-detail/document-detail.component.ts
+++ week-four/cms/src/app/documents/document-detail/document-detail.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-detail',
   templateUrl: './document-detail.component.html',
   styleUrls: ['./document-detail.component.scss']
 })
-export class DocumentDetailComponent {
+export class DocumentDetailComponent implements OnInit {
+  @Input() document: Document;
 
+  constructor() { }
+
+  ngOnInit(): void { }
 }

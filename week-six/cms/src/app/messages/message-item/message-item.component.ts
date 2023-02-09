2ce0d2dadeba590a436ed75f72154bedbab8b86b
keviner99 <90742482+keviner99@users.noreply.github.com>
2023-02-09 18:55:03 -0500

    add files from week 5

---
+++ week-six/cms/src/app/messages/message-item/message-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Message } from '../message.model';
+import { ContactService } from 'src/app/contacts/contact.service';
+
+
+@Component({
+  selector: 'app-message-item',
+  templateUrl: './message-item.component.html',
+  styleUrls: ['./message-item.component.scss']
+})
+export class MessageItemComponent implements OnInit {
+  @Input() message: Message;
+  messageSender: string;
+
+  constructor(private contactService: ContactService) { }
+  
+  ngOnInit(): void {
+    this.messageSender = this.contactService.getContact(
+      this.message.sender
+    ).name;
+      
+  }
+
+}

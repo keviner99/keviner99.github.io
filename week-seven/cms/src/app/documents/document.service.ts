f13090ade8b2973f7e7c181f8c59a2d3f0546ff6
keviner99 <90742482+keviner99@users.noreply.github.com>
2023-02-16 19:06:23 -0500

    create and subscribe to a subject to update

---
+++ week-seven/cms/src/app/documents/document.service.ts
@@ -0,0 +1,36 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Document } from './document.model';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+
+  selectedDocumentEvent = new EventEmitter<Document>();
+  documentChangedEvent = new EventEmitter<Document[]>();
+  documentListChangedEvent = new Subject<Document[]>();
+
+  private documents: Document[] = [];
+
+  constructor() { 
+    this.documents = MOCKDOCUMENTS;
+  }
+
+  getDocuments(): Document[] {
+    return this.documents.slice();
+  }
+
+  getDocument(id: string): Document {
+    return this.documents.find((d) => d.id === id);
+  }
+
+  deleteDocument(document: Document) {
+    if (!document) return;
+    const pos = this.documents.indexOf(document);
+    if (pos < 0) return;
+    this.documents.splice(pos, 1);
+    this.documentChangedEvent.emit(this.documents.slice());
+  }
+}

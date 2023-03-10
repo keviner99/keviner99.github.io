2df9aab1c340f87b05757b31fb62662f6020fd0a
keviner99 <90742482+keviner99@users.noreply.github.com>
2023-03-09 22:59:47 -0500

    add week 10 template

---
+++ week-ten/cms/src/app/documents/document.service.ts
@@ -0,0 +1,105 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { Document } from './document.model';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+
+  selectedDocumentEvent = new EventEmitter<Document>();
+  documentChangedEvent = new EventEmitter<Document[]>();
+  documentListChangedEvent = new Subject<Document[]>();
+
+  private documentsUrl = 'https://my-first-project-f9113-default-rtdb.firebaseio.com/documents.json';
+  private documents: Document[] = [];
+  private maxDocumentId: number;
+
+  constructor(private http: HttpClient) { 
+    this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
+  }
+  // GET REQUEST
+  getDocuments(): Document[] {
+    this.http
+      .get<Document[]>(this.documentsUrl)
+      .subscribe((docs: Document[]) => {
+        this.documents = docs;
+        this.maxDocumentId = this.getMaxId();
+        this.documents.sort((a, b) => {
+          if (a < b) return -1;
+          if (a > b) return 1;
+          return 0;
+        });
+        this.documentListChangedEvent.next(this.documents.slice());
+      });
+
+    return this.documents.slice();
+  }
+
+  //PUT REQUEST
+  storeDocuments() {
+    this.http
+      .put(this.documentsUrl, JSON.stringify(this.documents), {
+        headers: new HttpHeaders().set('Content-Type', 'application/json'),
+      })
+      .subscribe(() => {
+        this.documents.sort((a, b) => {
+          if (a < b) return -1;
+          if (a > b) return 1;
+          return 0;
+        });
+        this.documentListChangedEvent.next(this.documents.slice());
+      });
+  }
+
+
+  //CRUD
+  getDocument(id: string): Document {
+    return this.documents.find((d) => d.id === id);
+  }
+
+  deleteDocument(document: Document) {
+    if (!document) return;
+    const pos = this.documents.indexOf(document);
+    if (pos < 0) return;
+    this.documents.splice(pos, 1);
+    this.storeDocuments();
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    this.documents.forEach((d) => {
+      if (+d.id > maxId) maxId = +d.id;
+    });
+    return maxId;
+  }
+
+  addDocument(newDoc: Document) {
+    if (newDoc === null || newDoc === undefined) return;
+    
+    this.maxDocumentId++;
+    newDoc.id = `${this.maxDocumentId}`;
+    this.documents.push(newDoc);
+    this.storeDocuments();
+  }
+
+  updateDocument(original: Document, newDoc: Document) {
+    if (
+      newDoc === null ||
+      newDoc === undefined ||
+      original === null ||
+      original === undefined
+    ) {
+      return;
+    }
+    const pos = this.documents.indexOf(original);
+    if (pos < 0) return;
+
+    newDoc.id = original.id;
+    this.documents[pos] = newDoc;
+    this.storeDocuments();
+  }
+}

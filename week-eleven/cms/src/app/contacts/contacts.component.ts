b7ad591a13af280b3a34fa957741342038be6c38
keviner99 <90742482+keviner99@users.noreply.github.com>
2023-03-14 23:00:23 -0500

    add week 11 cms project

---
+++ week-eleven/cms/src/app/contacts/contacts.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './contact.model';
+import { ContactService } from './contact.service';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.scss']
+})
+export class ContactsComponent implements OnInit{
+
+  selectedContact: Contact;
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.contactService.contactSelectedEvent.subscribe((contact: Contact) => {
+      this.selectedContact = contact;
+    });
+  }
+
+}
